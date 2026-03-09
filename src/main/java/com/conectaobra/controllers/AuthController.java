c3849b9c7ec362778c041a7166f061e000b5efa3
AndreLuisGomes <andreluisgomes09@gmail.com>
2026-03-09 01:31:18 -0400

    feat(auth): refatorando códigos e adicionando novo endpoint para refreshToken

--- src/main/java/com/conectaobra/controllers/AuthController.java
+++ src/main/java/com/conectaobra/controllers/AuthController.java
@@ -1,32 +1,25 @@
 package com.conectaobra.controllers;
 
-import com.conectaobra.common.exceptions.NomeIndisponivelException;
-import com.conectaobra.common.exceptions.RoleInexistenteException;
-import com.conectaobra.common.exceptions.UsuarioSenhaInvalidosException;
-import com.conectaobra.dtos.AuthResponse;
-import com.conectaobra.dtos.UsuarioDTO;
-import com.conectaobra.dtos.UsuarioLoginDTO;
+import com.conectaobra.dtos.*;
+import com.conectaobra.exceptions.NomeIndisponivelException;
+import com.conectaobra.exceptions.RoleInexistenteException;
+import com.conectaobra.exceptions.UsuarioSenhaInvalidosException;
 import com.conectaobra.models.Role;
 import com.conectaobra.models.Usuario;
 import com.conectaobra.services.RoleService;
+import com.conectaobra.services.TokenService;
 import com.conectaobra.services.UsuarioService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.constraints.Length;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
 import java.util.Optional;
 
 @RestController
@@ -36,20 +29,26 @@ import java.util.Optional;
 public class AuthController {
 
     // Dependências \\
-    public final String apiName;
 
-    private final JwtEncoder jwtEncoder;
     private final PasswordEncoder passwordEncoder;
 
+    private final TokenService tokenService;
     private final UsuarioService usuarioService;
     private final RoleService roleService;
 
     // JWT Constantes \\
 
-    private final long AUTH_REFRESH_TOKEN = 86400L;
-    private final long AUTH_TOKEN = 300L;
+    public final long AUTH_TOKEN;
+    public final long AUTH_REFRESH_TOKEN;
+    public final String TOKEN_TYPE = "Bearer ";
 
     // Método de login \\
+
+    @PostMapping("refresh-token")
+    ResponseEntity<Object> refreshToken(@RequestBody @Valid RefreshToken refreshToken){
+        RefreshTokenResponse authToken = this.tokenService.validarJWT(refreshToken);
+        return authToken != null ? ResponseEntity.ok().body(authToken) : ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Deu errado" + authToken);
+    }
 
     @PostMapping("logar")
     ResponseEntity<Object> logar(@RequestBody @Valid UsuarioLoginDTO usuarioLoginDTO) throws Exception{
@@ -65,8 +64,10 @@ public class AuthController {
         ){
             return ResponseEntity.ok().body(
                     new AuthResponse(usuarioLogin.nome(),
-                    this.gerarJWT(usuarioDoBanco.get(), AUTH_TOKEN).getTokenValue(),
-                    this.gerarJWT(usuarioDoBanco.get(), AUTH_REFRESH_TOKEN).getTokenValue(),
+                    TOKEN_TYPE,
+                    (int) AUTH_TOKEN,
+                    this.tokenService.gerarJWT(usuarioDoBanco.get(), AUTH_TOKEN).getTokenValue(),
+                    this.tokenService.gerarJWT(usuarioDoBanco.get(), AUTH_REFRESH_TOKEN).getTokenValue(),
                     usuarioDoBanco.get().getRole().getNome()
                     ));
         }
@@ -90,30 +91,14 @@ public class AuthController {
             novoUsuario.setSenha(this.passwordEncoder.encode(novoUsuario.getSenha()));
             this.usuarioService.salvarUsuario(novoUsuario);
             return ResponseEntity.ok().body(
-                    new AuthResponse(novoUsuario.getNome(),
-                            this.gerarJWT(novoUsuario, this.AUTH_TOKEN).getTokenValue(),
-                            this.gerarJWT(novoUsuario, this.AUTH_REFRESH_TOKEN).getTokenValue(),
+                    new AuthResponse(
+                            novoUsuario.getNome(),
+                            TOKEN_TYPE,
+                            (int) AUTH_TOKEN,
+                            this.tokenService.gerarJWT(novoUsuario, AUTH_TOKEN).getTokenValue(),
+                            this.tokenService.gerarJWT(novoUsuario, AUTH_REFRESH_TOKEN).getTokenValue(),
                             novoUsuario.getRole().getNome()));
         }
         throw new NomeIndisponivelException("Nome indisponível!", "nome");
-    }
-
-    // Método para gerar JWT \\
-
-    private Jwt gerarJWT(Usuario usuario, long exp){
-        Instant now = Instant.now();
-
-        String scope = usuario.getRole().getNome();
-
-        // Claims: são os "componentes" que o JwtEncoder aceita para sua construção \\
-        JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer(this.apiName)
-                .subject(usuario.getId().toString())
-                .issuedAt(now)
-                .expiresAt(now.plusSeconds(exp))
-                .claim("scope", scope)
-                .build();
-
-        return jwtEncoder.encode(JwtEncoderParameters.from(claims));
     }
 }
--- src/main/java/com/conectaobra/dtos/AuthResponse.java
+++ src/main/java/com/conectaobra/dtos/AuthResponse.java
@@ -1,6 +1,8 @@
 package com.conectaobra.dtos;
 
 public record AuthResponse(String nome,
+                           String tokenType,
+                           int expiresIn,
                            String accessToken,
                            String refreshToken,
                            String role) {
